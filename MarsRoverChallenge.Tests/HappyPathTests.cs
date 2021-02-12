1a5b7e786d3e59ccdb210067af65b8d2aca7b976
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 06:44:44 +0200

    SpinTests added inline data for all possible combinations

--- MarsRoverChallenge.Tests/HappyPathTests.cs
+++ MarsRoverChallenge.Tests/HappyPathTests.cs
@@ -40,6 +40,12 @@ namespace MarsRoverChallenge.Tests
         [Theory]
         [InlineData(5, 5, "N", 'L', "5 5 W")] //heading north, spin left, expected heading West
         [InlineData(5, 5, "N", 'R', "5 5 E")] //heading north, spin right, expected heading East
+        [InlineData(5, 5, "E", 'L', "5 5 N")]
+        [InlineData(5, 5, "E", 'R', "5 5 S")]
+        [InlineData(5, 5, "S", 'L', "5 5 E")]
+        [InlineData(5, 5, "S", 'R', "5 5 W")]
+        [InlineData(5, 5, "W", 'L', "5 5 S")]
+        [InlineData(5, 5, "W", 'R', "5 5 N")]
         public void Rover_ExecuteCommand_spin_faces_new_direction(int x, int y, string heading, Char command, string expected)
         {
             //arrange
