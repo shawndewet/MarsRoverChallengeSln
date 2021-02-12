7b499d93388d48be10dec5a504f3cff563d8a82d
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 08:25:11 +0200

    Collision tests...added inline data for all directions...tests pass.

--- MarsRoverChallenge.Tests/CollisionTests.cs
+++ MarsRoverChallenge.Tests/CollisionTests.cs
@@ -6,11 +6,14 @@ namespace MarsRoverChallenge.Tests
 {
     public class CollisionTests
     {
-        [Theory]
-        [InlineData(1,2,"E","1 2 E")]
+        [Theory] //none of these should move
+        [InlineData(1, 2, "E", "1 2 E")] //to the Left, facing East
+        [InlineData(3, 2, "W", "3 2 W")] //to the Right, facing West
+        [InlineData(2, 1, "N", "2 1 N")] //to the South, facing North
+        [InlineData(2, 3, "S", "2 3 S")] //to the North, facing South
         public void Rover_ExecuteCommand_ignores_command_that_would_cause_collision_with_another_rover(int x, int y, string heading, string expected)
         {
-            //arrange
+            //arrange an existing rover at x,y 2,2
             var otherRovers = new Library.Rover[]{
                 new Library.Rover(5, 5, 2, 2, "N")
             };
