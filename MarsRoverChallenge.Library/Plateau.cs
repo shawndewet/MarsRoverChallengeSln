c8085c1b4e85ba72a18a5bea358de4248935af7d
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 06:30:11 +0200

    Plateau.Process implemented, but test failing due to non-implemented Rover.ExecuteCommand

--- MarsRoverChallenge.Library/Plateau.cs
+++ MarsRoverChallenge.Library/Plateau.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace MarsRoverChallenge.Library
 {
@@ -6,7 +8,33 @@ namespace MarsRoverChallenge.Library
     {
         public static string[] Process(string[] instructions)
         {
-            throw new NotImplementedException();
+            //subtract 1 to ignore the first instruction (plateau size)
+            //divide by two because each rover has two lines of input
+            var roverCount = (instructions.Length - 1) / 2;
+
+            //stores a list of rovers that appear in the instructions
+            var rovers = new Rover[roverCount - 1];
+
+            for (int roverNumber = 1; roverNumber <= roverCount; roverNumber++)
+            {
+                var roverLocation = instructions[roverNumber];
+                var roverCommands = instructions[roverNumber + 1];
+
+                rovers[roverNumber - 1] = new Rover(
+                    int.Parse(roverLocation.Split(' ')[0]),
+                    int.Parse(roverLocation.Split(' ')[1]),
+                    roverLocation.Split(' ')[2]
+                    );
+
+                foreach (var command in roverCommands)
+                    rovers[roverNumber - 1].ExecuteCommand(command);
+            }
+
+            var output = new string[roverCount];
+            for (int roverNumber = 1; roverNumber <= roverCount; roverNumber++)
+                output[roverNumber - 1] = rovers[roverNumber - 1].ReportPosition();
+
+            return output;
         }
     }
 }
---
+++ MarsRoverChallenge.Library/Rover.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverChallenge.Library
+{
+    public class Rover
+    {
+        public Rover(int x, int y, string heading)
+        {
+            X = x;
+            Y = y;
+            Heading = heading;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string Heading { get; private set; }
+
+        internal void ExecuteCommand(char command)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string ReportPosition()
+        {
+            return $"{X} {Y} {Heading}";
+        }
+    }
+}
