628986c8ec6f2dcde0c878e47a6bd234e5e1036a
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 07:50:50 +0200

    Added failing collision test

---
+++ MarsRoverChallenge.Tests/CollisionTests.cs
@@ -0,0 +1,26 @@
+using Shouldly;
+using System;
+using Xunit;
+
+namespace MarsRoverChallenge.Tests
+{
+    public class CollisionTests
+    {
+        [Theory]
+        [InlineData(1,2,"E","1 2 E")]
+        public void Rover_ExecuteCommand_ignores_command_that_would_cause_collision_with_another_rover(int x, int y, string heading, string expected)
+        {
+            //arrange
+            var staticRover = new Library.Rover(5, 5, 2, 2, "N");
+            var roverToMove = new Library.Rover(5, 5, x, y, heading);
+
+            //act
+            roverToMove.ExecuteCommand('M');
+
+            //assert
+            var actual = roverToMove.ReportPosition();
+            actual.ShouldBe(expected);
+        }
+
+    }
+}
