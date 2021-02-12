26d9ad49b08c401594a6a9982722cdff818935ca
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 05:28:32 +0200

    Add project files.

---
+++ MarsRoverChallenge.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MarsRoverChallenge.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ MarsRoverChallenge.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MarsRoverChallenge.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
