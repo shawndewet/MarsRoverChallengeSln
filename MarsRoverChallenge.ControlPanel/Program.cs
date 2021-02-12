7f3c7cd9bd3efc673071835c06ad8d95133df9f8
Shawn de Wet <shawndewet@codewell.co.za>
2021-02-12 08:43:15 +0200

    Added basic Console UI.

---
+++ MarsRoverChallenge.ControlPanel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRoverChallenge.ControlPanel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new List<string>();
+            Console.WriteLine("Welcome to the Mars Rover control panel!");
+            Console.WriteLine("Enter your sequence of commands to control the rovers.");
+            Console.WriteLine("(Signify the end of your input by entering an empty row)");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                    commands.Add(input);
+            } while (!string.IsNullOrEmpty(input));
+
+            Console.WriteLine("Sending commands to rovers...");
+            Console.WriteLine();
+
+            var output = Library.Plateau.Process(commands.ToArray());
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
